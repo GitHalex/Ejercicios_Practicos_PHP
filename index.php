a27d861881c59891203af9b2b90376fabed62656
GitHalex <calex1731@gmail.com>
2024-09-15 02:46:25 -0400

    primeros pasos en php no me esta gustando para nada

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1>Mi primer fichero en PHP</h1>
+  <?php $nombre = "Alex";?>
+  <h1>
+    <?= $nombre;?>
+  </h1>
+</body>
+</html>
